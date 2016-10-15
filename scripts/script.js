fd7e1544a80954fc8358ec60e6f59930ff678362
mor7 <lxdsmort@gmail.com>
2016-10-15 14:36:42 +0200

    load data from backend

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
-var map = L.map('map').setView([-37.87, 175.475], 12);
+var map = L.map('map').setView([52, 13], 4);
 var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
-addressPoints = addressPoints.map(function (p) { return [p[0], p[1]]; });
-var heat = L.heatLayer(addressPoints).addTo(map);
+
+var heat = null;
+
+$.ajax({
+    url: "http://tmp.pajowu.de/api/data/?format=json",
+    type: "GET",
+    dataType: 'json',
+    success: function(result) {
+        var points = [];
+        for (var i = 0; i < result.length; i++) {
+            var point = result[i]["location"]["coordinates"];
+
+            for (var j = 0; j < result[i]["count"]; j++)
+                points.push(point);
+        }
+
+        heat = L.heatLayer(points, {radius: 20, maxZoom: 10}).addTo(map);
+    }
+});

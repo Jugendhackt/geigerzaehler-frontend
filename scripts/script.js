50afe32afa056f43ba97483f78011abe51fd2889
sevenmaster <seven-master@hotmail.de>
2016-10-15 17:22:26 +0200

    added scale

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,13 @@ var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var heat = null;
+function scale(ms) {
+    var push;
+    push = ms / 15;
+    if (push > 1)
+        push = 1;
+    return push;
+}
 
 $.ajax({
     url: "http://tmp.pajowu.de/api/data/?format=json",
@@ -13,11 +20,13 @@ $.ajax({
         var points = [];
         for (var i = 0; i < result.length; i++) {
             var point = result[i]["location"]["coordinates"];
+            point.push(scale(result[i]["mSv"]));
 
-            for (var j = 0; j < result[i]["count"]; j++)
+            //for (var j = 0; j < result[i]["count"]; j++)
                 points.push(point);
+            //Maxwert 15 Millisivert
         }
 
-        heat = L.heatLayer(points, {radius: 20, maxZoom: 10}).addTo(map);
+        heat = L.heatLayer(points, {radius: 20, maxZoom: 5}).addTo(map);
     }
 });
